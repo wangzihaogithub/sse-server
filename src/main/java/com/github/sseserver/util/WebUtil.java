0b0231c1860d569a1242bee48643129eaeab020a
wangzihaogithub <hao.wang@iterget.com>
2023-02-14 12:54:58 +0800

    1.2.3 1.解决多网卡选择错了网卡

--- src/main/java/com/github/sseserver/util/WebUtil.java
+++ src/main/java/com/github/sseserver/util/WebUtil.java
@@ -130,6 +130,18 @@ public class WebUtil {
                     }
                 }
 
+                for (String ip : ipList) {
+                    if (ip.startsWith("172.")) {
+                        return ip;
+                    }
+                }
+                for (String ip : ipList) {
+                    // xxx.
+                    // xx.
+                    if (ip.length() > 3 && ip.charAt(3) == '.') {
+                        return ip;
+                    }
+                }
                 // 如果没有发现 non-loopback地址.只能用最次选的方案
                 if (ipList.isEmpty()) {
                     ipList.addLast(InetAddress.getLocalHost().getHostAddress());
