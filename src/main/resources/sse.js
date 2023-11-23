e6d80a366c8b9a6e35d603e921380efaf5f2a21a
wangzihaogithub <hao.wang@iterget.com>
2023-11-23 10:15:52 +0800

    1.2.12 缩短RedisServiceDiscoveryService的redis-key

--- src/main/resources/sse.js
+++ src/main/resources/sse.js
@@ -9,11 +9,11 @@
  *   <dependency>
  *      <groupId>com.github.wangzihaogithub</groupId>
  *      <artifactId>sse-server</artifactId>
- *      <version>1.2.11</version>
+ *      <version>1.2.12</version>
  *   </dependency>
  */
 class Sse {
-  static version = '1.2.11'
+  static version = '1.2.12'
   static DEFAULT_OPTIONS = {
     url: '/api/sse',
     keepaliveTime: 900000,
